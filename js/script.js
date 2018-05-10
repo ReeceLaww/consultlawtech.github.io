242010173013ede76c282eeedc7d33975211f87e
Reece Law <reecealaw@gmail.com>
2018-05-10 12:10:18 +0100

    The start of a website
    Massive changes to website, starting of google form integration

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-<script>
-    document.getElementById('vid').play();
-</script>
+// Auto video player
+document.getElementById('vid').play();
+
+function scrollFunction(){
+  document.body.scrollTop = 1005;
+  document.documentElement.scrollTop = 1005;
+}
